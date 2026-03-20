fae795eaf7aa34b79590d8601944dd7b45f3b4d5
anmolsingh7121 <anmolsingh7121@gmail.com>
2026-03-21 03:24:58 +0530

    Made Some minor changes in complaint.js file

--- src/models/complaint.js
+++ src/models/complaint.js
@@ -1,7 +1,8 @@
 import complaints from "../test/testcases.js";  // importing test cases from another file
 
 
-var processedIds = [];  // array needs to be declared before it is getting used 
+let processedIds = [];  // array needs to be declared before it is getting used 
+var validComplaints = []
 complaints.forEach((complaint)=>{  // looping over each complaint from the complaints array 
     isValidComplaint(complaint);
 })
@@ -18,33 +19,33 @@ function isValidComplaint(complaint){
     }
 
     if (processedIds.includes(complaint.complaint_id)){
-        console.log(complaint.complaint_id)
+        // console.log(complaint.complaint_id)
         return {validation: false , reason: "Duplicate complaint id"}
     }
 
     if(!["payment", "technical","account", "delivery"].includes(complaint.issue_type)){
-        console.log(complaint.issue_type)
+        // console.log(complaint.issue_type)
         return {validation: false, reason: "Issue type wrong"}
     }
 
     if(!["high", "medium", "low"].includes(complaint.priority)){
-        console.log(complaint.priority)
+        // console.log(complaint.priority)
         return {validation: false, reason: "Priority type wrong"}
     }
 
     if(!["premium", "regular"].includes(complaint.customer_type)){
-        console.log(complaint.customer_type)
-        return {validation: false, reason: "Priority type wrong"}
+        // console.log(complaint.customer_type)
+        return {validation: false, reason: "Invalid customer type"}
     }
 
 
-    if(!/^\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}$/.test(complaint.created_time)){
-        console.log(complaint.created_time)
+    if(!/^\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}$/.test(complaint.created_time) || new Date(complaint.created_time) > new Date()){
+        // console.log(complaint.created_time)
         return {validation: false, reason:"Invalid Time Stamp"}
     }
 
     if(!["resolved", "unresolved"].includes(complaint.status)){
-        console.log(complaint.status)
+        // console.log(complaint.status)
         return {validation: false, reason:"Invalid Status"}
     }
 
@@ -54,3 +55,12 @@ function isValidComplaint(complaint){
 
 
 }
+
+complaints.forEach((complaint)=>{
+    if(processedIds.includes(complaint.complaint_id)){
+        validComplaints.push(complaint);
+        processedIds = processedIds.filter((id)=> id!== complaint.complaint_id)
+    }
+})
+
+export default validComplaints;
